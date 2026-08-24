2e792371e4a5d5d52a41a86121c1a539c8c07d27
spiral <97077158+Someone68@users.noreply.github.com>
2026-08-24 16:31:21 -0700

    add player movement

---
+++ scenes/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	
+	move_and_slide()
