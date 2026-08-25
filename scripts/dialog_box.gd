2ab8035b911e7a6b901b17f76ef930d0b87f327d
spiral <97077158+Someone68@users.noreply.github.com>
2026-08-24 22:27:33 -0700

    update dialog box

--- scripts/dialog_box.gd
+++ scripts/dialog_box.gd
@@ -1,21 +1,43 @@
 extends CanvasLayer
 
 var dialog_visible := false
+var _skip_requested := false
 
-func show_dialog(text: String, speed: float = 0.04, pause_multiplers := [2.5, 3]):
+func _input(event: InputEvent) -> void:
+	if dialog_visible and event.is_action_pressed("cancel"):
+		_skip_requested = true
+		get_viewport().set_input_as_handled()
+
+func show_dialog(dialogue: Array, speed: float = 0.04, pause_multiplers := [2.5, 3]):
 	dialog_visible = true
-	$Control/Text.text = ""
 	$Control.visible = true
-	for c in text:
-		$Control/Text.text += c
-		var wait_time = speed
-		if (c in [';', ',', ':']):
-			wait_time *= pause_multiplers[0]
-		elif (c in ['.', '?', '!']):
-			wait_time *= pause_multiplers[1]
-		await get_tree().create_timer(wait_time).timeout
+	_skip_requested = false
+	for text in dialogue:
+		$Control/Text.text = ""
+		for c in text:
+			if _skip_requested:
+				$Control/Text.text = text
+				break
+			$Control/Text.text += c
+			var wait_time = speed
+			if (c in [';', ',', ':']):
+				wait_time *= pause_multiplers[0]
+			elif (c in ['.', '?', '!']):
+				wait_time *= pause_multiplers[1]
+			await get_tree().create_timer(wait_time).timeout
+		$Icon.visible = true
+		_skip_requested = false
+		
+		while not Input.is_action_just_pressed("interact"):
+			await get_tree().process_frame
+		
+		$Icon.visible = false
+	
+	dialog_visible = false
+	$Control.visible = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and dialog_visible:
-		dialog_visible = false
-		$Control.visible = false
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("interact") and dialog_visible:
+		#dialog_visible = false
+		#$Control.visible = false
+		#$Icon.visible = false
--- scripts/root.gd
+++ scripts/root.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(2).timeout
-	$DialogBox.show_dialog("testetstest. testtest, testestestes! test")
+	$DialogBox.show_dialog(["testetstest. testtest, testestestes! test. this is some very long text to test the dialog.", "this is more text to test multiple dialogs."])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
