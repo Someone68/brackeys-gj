a6957a7fa1827b4956aeb534588e38829a60d6f2
spiral <97077158+Someone68@users.noreply.github.com>
2026-08-24 17:25:12 -0700

    add interactables

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@export var INTERACT_RANGE := 24.0
+@export var FEET_OFFSET := Vector2(0, 0)
+
+@onready var ray: RayCast2D = $RayCast2D
+var facing = Vector2(1, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +15,28 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	if (not input_direction.is_zero_approx()):
+		facing = input_direction.normalized()
+		ray.target_position = facing * INTERACT_RANGE
 	velocity = input_direction * speed
 	
 	move_and_slide()
+	
+
+func update_ray() -> void:
+	ray.target_position = facing * INTERACT_RANGE
+	if abs(facing.x) > abs(facing.y):
+		ray.position = FEET_OFFSET
+	else:
+		ray.position = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		try_interact()
+
+func try_interact() -> void:
+	if not ray.is_colliding():
+		return
+	var c = ray.get_collider()
+	if c.is_in_group("interactable") and c.has_method("interact"):
+		c.interact()
